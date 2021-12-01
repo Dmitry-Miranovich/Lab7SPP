acb3e7121194c82e50b55b1274413f520a43566d
Dmitry_Miranovich <miranovich.2001@yandex.ru>
2021-12-01 09:42:20 +0300

    Lab 7 is done

---
+++ Lab7SPP/Lab7SPP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Lab1SPP.Elements;
+
+namespace Lab7SPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CParallel p = new CParallel();
+            TaskDelegate[] mas = new TaskDelegate[]
+            {
+                new TaskDelegate(()=>{ Console.WriteLine("Task 1"); }),
+                new TaskDelegate(()=>{ Console.WriteLine("Task 2"); }),
+                new TaskDelegate(()=>{ Console.WriteLine("Task 3"); }),
+                new TaskDelegate(()=>{ Console.WriteLine("Task 4"); }),
+                new TaskDelegate(()=>{ Console.WriteLine("Task 5"); }),
+                new TaskDelegate(()=>{ Console.WriteLine("Task 6");
+                Thread.Sleep(5000); }),
+                new TaskDelegate(()=>{ Console.WriteLine("Task 7"); }),
+                new TaskDelegate(()=>{ Console.WriteLine("Task 8"); }),
+                new TaskDelegate(()=>{ Console.WriteLine("Task 9"); })
+            };
+
+            p.WaitAll(mas);
+            Console.WriteLine("Delegates are executed, if this message was printed last, then WaitAll work properly");
+            Console.ReadKey();
+        }
+    }
+}
